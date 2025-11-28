4a8fd2c215969f642e23be7bd216fca1f684d7c8
Greg Brail <gbrail@users.noreply.github.com>
2025-11-28 10:42:04 -0800

    Basic framework for running tests

---
+++ test-framework/src/test/java/org/brail/whatjs/framework/tests/SmokeTest.java
@@ -0,0 +1,66 @@
+package org.brail.whatjs.framework.tests;
+
+import org.brail.whatjs.framework.WPTTestLauncher;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.JavaScriptException;
+import org.mozilla.javascript.Scriptable;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SmokeTest {
+    private static Context cx;
+    private static WPTTestLauncher launcher;
+
+    @BeforeAll
+    public static void init() throws IOException {
+        cx = Context.enter();
+        Scriptable scope = cx.initStandardObjects();
+        launcher = WPTTestLauncher.newLauncher(cx, scope);
+    }
+
+    @Test
+    public void basicTest() {
+        launcher.runScript(cx, """
+      test(() => {
+        assert_true(true);
+      }, "Working test function");
+      """);
+    }
+
+    @Test
+    public void failingTest() {
+        assertThrows(JavaScriptException.class, () -> {
+            launcher.runScript(cx, """
+                    test(() => {
+                      assert_true(false);
+                    }, "Failing test function");
+                    """);
+        });
+    }
+
+    @Test
+    public void basicPromiseTest() {
+        launcher.runScript(cx, """
+      promise_test(() => {
+        return new Promise((resolve) => {
+          resolve();
+        });
+      }, "Working promise test function");
+      """);
+    }
+
+    @Test
+    public void failingPromiseTest() {
+        launcher.runScript(cx, """
+      promise_test(() => {
+        return new Promise((resolve, reject) => {
+          reject('promise not kept');
+        });
+      }, "Working promise test function");
+      """);
+    }
+}
