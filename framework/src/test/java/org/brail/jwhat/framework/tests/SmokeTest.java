c5ba89bebf1d97c8b6ea5d35721b4a070799b302
Greg Brail <gbrail@users.noreply.github.com>
2026-01-08 20:37:28 -0800

    Spotless fix

--- framework/src/test/java/org/brail/jwhat/framework/tests/SmokeTest.java
+++ framework/src/test/java/org/brail/jwhat/framework/tests/SmokeTest.java
@@ -1,6 +1,8 @@
 package org.brail.jwhat.framework.tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import org.brail.jwhat.framework.WPTTestLauncher;
